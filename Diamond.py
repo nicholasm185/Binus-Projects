6e686c1b21c86703f7b88031329fc2731e081fe6
nicholasm185 <43334128+nicholasm185@users.noreply.github.com>
2018-09-22 21:56:40 +0700

    improvements

--- Diamond.py
+++ Diamond.py
@@ -1,22 +1,28 @@
 # user input for the height of the first half
-height = int(input("Input any number: "))
+while True:
+    try:
+        height = int(input("Input any number: "))
+        break
+    except ValueError:
+        print("Please input integers")
+
+# tell user the height of diamond
+print("Your diamond will have the height of: "+str(height*2-1))
 
 # variables
-# height1 used for second half
-height1 = height + 1
+# height1 used for second half counter
+height1 = height
 stars = 1
-# stars1 used for second half
-stars1 = 1
 
 # first half of diamond
-while height > 0:
+while height > 1:
     print(" " * height + "*" * stars + "*" *(stars-1))
     stars += 1
     height -= 1
 
 # second half of diamond
 while height1 > 0:
-    print(" " * (height) + "*" * stars + "*" * (stars-1))
+    print(" " * height + "*" * stars + "*" * (stars-1))
     stars -= 1
     height += 1
     height1 -= 1
--- Triangle.py
+++ Triangle.py
@@ -1,6 +1,6 @@
 num = 0
 count = int(input("How big is your triangle: "))
-star = int(input("Length of your \"triangle\": "))
+star = int(input("Top of your \"triangle\": "))
 while num < count:
     print("*"*star)
     star += 1
